8e0b42d80de9462b4e1705cf1d4d6ef98b6e4b13
Diego Flores <defsdiego@gmail.com>
2021-07-29 15:34:36 -0700

    added wave increase control

--- src/canvas.js
+++ src/canvas.js
@@ -1,8 +1,10 @@
+//get canvas context and resize window
 let canvas = document.getElementById('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let c = canvas.getContext('2d');
 
+//vars
 let x, y;
 let amp = 20;
 let wLength = 0.005;
@@ -27,11 +29,28 @@ rangeFreq.max = 400;
 rangeLength.step = 0.1;
 rangeFreq.defaultValue = transferFreq * 100;
 
+const rangeIncrease = document.getElementById("rangeIncrease");
+rangeIncrease.min = 0;
+rangeIncrease.max = 500;
+rangeIncrease.step = 5;
+rangeIncrease.defaultValue = 100;
+
+//checkboxes
+const checkIncrease = document.getElementById("checkIncrease");
+checkIncrease.defaultChecked = false;
+
+//event listeners
 window.addEventListener('resize', () => {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-})
+});
 
+rangeIncrease.hidden = true;
+checkIncrease.addEventListener('click', () => {
+    rangeIncrease.hidden = !rangeIncrease.hidden;
+});
+
+//custom functions
 function drawLine(x, y, endX, endY) {
     c.beginPath();
     c.moveTo(x, y);    
@@ -40,8 +59,8 @@ function drawLine(x, y, endX, endY) {
     c.closePath();
 }
 
+//setup and draw () => {}
 function setup() {
-
 
     window.requestAnimationFrame(draw);
 }
@@ -55,17 +74,23 @@ function draw() {
     for(let i = 0; i < canvas.width; i++) {
         c.strokeStyle = 'rgba(130, 200, 255)';
         
+        let nextPointY = checkIncrease.checked ?
+        (canvas.height / 2) + 
+        Math.sin(-i*(parseFloat(rangeLength.value)/100) + transfer) * 
+        parseInt(rangeAmp.value) * i/parseFloat(rangeIncrease.value)
+        :
+        (canvas.height / 2) + 
+        Math.sin(-i*(parseFloat(rangeLength.value)/100) + transfer) * 
+        parseInt(rangeAmp.value)
+        ;
+        
         c.lineTo(
             i, 
-            (canvas.height / 2) + 
-            Math.sin(-i*(parseFloat(rangeLength.value)/100) + transfer) * 
-            parseInt(rangeAmp.value) * i/100);
+            nextPointY);
         c.stroke();
         drawLine(
             i, 
-            (canvas.height / 2) + 
-            Math.sin(-i*(parseFloat(rangeLength.value)/100) + transfer) * 
-            parseInt(rangeAmp.value) * i/100, 
+            nextPointY, 
             i, 
             canvas.height)
     }
@@ -76,4 +101,5 @@ function draw() {
     window.requestAnimationFrame(draw);
 }
 
+//draw execution
 setup();
